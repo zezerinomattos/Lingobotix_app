c9e92c9b4ad1ea3e786f612c42205cb535987227
zezerinodev <mattoszz@hotmail.com>
2023-05-13 18:15:39 -0300

    Tela para fazer login foi projetada e está aguardando para criar a função

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+
+// MY IMPORTS
+import styles from './styles';
 
 
 export default function SignIn(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [message, setMessage] = useState('');
+
+    async function hendleLogin(){
+        alert(`email ${email} - password ${password}`);
+    }
+
     return(
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Text style={styles.textTitle}>
+                Login
+            </Text>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input} placeholder='Informe seu e-mail' placeholderTextColor={'#023E73'} value={email} onChangeText={setEmail}/>
+
+                <TextInput style={styles.input} secureTextEntry={true} placeholder='Informe sua senha' placeholderTextColor={'#023E73'} value={password} onChangeText={setPassword}/>
+
+                {message && <Text style={styles.message}>{message}</Text>}
+
+                <TouchableOpacity style={styles.button} onPress={hendleLogin}>
+                    <Text style={styles.textButton}>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Image source={require('../../images/LingoP.png')} resizeMode='contain' style={{width: 150}}/>
         </View>
     );
 }
---
+++ src/pages/SignIn/styles.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#023E73',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }, 
+    textTitle: {
+        fontSize: 35,
+        color: '#BF6415',
+        fontWeight: '800',
+        marginBottom: 20
+    },
+    inputContainer:{
+        width: '95%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 8,
+        marginTop: 20,
+    },
+    input: {
+        width: '95%',
+        height: 40,
+        backgroundColor: '#FFF',
+        marginBottom: 10,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        color: '#083359'
+    },
+    button:{
+        backgroundColor: '#BF6415',
+        width: '95%',
+        height: 40,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    textButton:{
+        color: '#FFF',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    message: {
+        color: '#F21616',
+        fontSize: 18,
+        textAlign: 'center',
+        marginBottom: 10,
+        fontWeight: '600'
+    }
+});
+
+export default styles;
