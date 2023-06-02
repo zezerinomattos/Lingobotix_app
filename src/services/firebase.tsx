1664657d0e5abac94c0fb45e169e082d5c7cd870
zezerinodev <mattoszz@hotmail.com>
2023-06-02 11:31:55 -0300

    Ajuste

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBX81WeBSkeOW14S2gzVyKZ7PnpXwFoaGo",
-    authDomain: "lingobotix-c7e29.firebaseapp.com",
-    projectId: "lingobotix-c7e29",
-    storageBucket: "lingobotix-c7e29.appspot.com",
-    messagingSenderId: "371082673827",
-    appId: "1:371082673827:web:e4fa51ba5ae6c271da4916",
-    measurementId: "G-Z5SP1M9LEN"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 if(!firebase.apps.length){
